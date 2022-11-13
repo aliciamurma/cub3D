da52edbc0d99bbb4a2da606f38c481415f392ce6
Alberto Ramirez <a.ramir.sa@gmail.com>
2022-11-13 17:31:28 +0100

    Update main.c

--- src/mandatory/main.c
+++ src/mandatory/main.c
@@ -1,28 +1,17 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: amurcia- <amurcia-@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/13 16:44:01 by amurcia-          #+#    #+#             */
-/*   Updated: 2022/11/13 17:24:50 by amurcia-         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <mlx.h>
-#include "cub3d.h"
 
-int	main(int argc, char **argv)
+int	main(void)
 {
 	void	*mlx;
-	// t_game	game;
+	void	*mlx_win;
+    int     dim;
 
-	check_parameters(argc, argv);
-	// get_cub_info(&game, argv);
-	// check_map(&game);
+    dim = 100;
 	mlx = mlx_init();
-	mlx_new_window(mlx, 500, 500, "Hello world!");
+	mlx_win = mlx_new_window(mlx, 500, 500, "Hello world!");
+
+    void *img = mlx_xpm_file_to_image(mlx, "../../assets/west.xpm", &dim, &dim);
+    mlx_put_image_to_window(mlx, mlx_win, img, 0, 0);
 	mlx_loop(mlx);
-    return (0);
+    return 0;
 }
