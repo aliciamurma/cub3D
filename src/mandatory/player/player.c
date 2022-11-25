8b6f79caf9ebd732552c7d6204af417158bbe852
Alberto Ramirez <57710147+arsalas@users.noreply.github.com>
2022-11-25 16:54:18 +0100

    norminette

--- src/mandatory/player/player.c
+++ src/mandatory/player/player.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   player.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramirez <aramirez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/25 16:49:20 by aramirez          #+#    #+#             */
+/*   Updated: 2022/11/25 16:54:06 by aramirez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <stdbool.h>
 #include <math.h>
@@ -5,68 +16,68 @@
 #include "game.h"
 #include "player.h"
 
-bool ft_can_move(char **map, double pos_x, double pos_y)
+bool	ft_can_move(char **map, double pos_x, double pos_y)
 {
-    if (map[(int)pos_x][(int)pos_y] == '0')
-        return (true);
-    return (false);
+	if (map[(int)pos_x][(int)pos_y] == '0')
+		return (true);
+	return (false);
 }
 
-static double ft_get_new_position(double dir)
+static double	ft_get_new_position(double dir)
 {
-    return (dir * SPEED);
+	return (dir * SPEED);
 }
 
-void ft_move_forward(t_player *player, char **map)
+void	ft_move_forward(t_player *player, char **map)
 {
-    double new_pos_x;
-    double new_pos_y;
+	double	new_pos_x;
+	double	new_pos_y;
 
-    new_pos_x = ft_get_new_position(player->dir.x);
-    new_pos_y = ft_get_new_position(player->dir.y);
-    if (ft_can_move(map, new_pos_x + player->pos.x, player->pos.y))
-        player->pos.x += new_pos_x;
-    if (ft_can_move(map, player->pos.x, new_pos_y + player->pos.y))
-        player->pos.y += new_pos_y;
+	new_pos_x = ft_get_new_position(player->dir.x);
+	new_pos_y = ft_get_new_position(player->dir.y);
+	if (ft_can_move(map, new_pos_x + player->pos.x, player->pos.y))
+		player->pos.x += new_pos_x;
+	if (ft_can_move(map, player->pos.x, new_pos_y + player->pos.y))
+		player->pos.y += new_pos_y;
 }
 
-void ft_move_backward(t_player *player, char **map)
+void	ft_move_backward(t_player *player, char **map)
 {
-    double new_pos_x;
-    double new_pos_y;
+	double	new_pos_x;
+	double	new_pos_y;
 
-    new_pos_x = ft_get_new_position(player->dir.x);
-    new_pos_y = ft_get_new_position(player->dir.y);
-    if (ft_can_move(map, new_pos_x + player->pos.x, player->pos.y))
-        player->pos.x -= new_pos_x;
-    if (ft_can_move(map, player->pos.x, new_pos_y + player->pos.y))
-        player->pos.y -= new_pos_y;
+	new_pos_x = ft_get_new_position(player->dir.x);
+	new_pos_y = ft_get_new_position(player->dir.y);
+	if (ft_can_move(map, new_pos_x + player->pos.x, player->pos.y))
+		player->pos.x -= new_pos_x;
+	if (ft_can_move(map, player->pos.x, new_pos_y + player->pos.y))
+		player->pos.y -= new_pos_y;
 }
 
-void ft_move_left(t_player *player, char **map)
+void	ft_move_left(t_player *player, char **map)
 {
-    double new_pos_x;
-    double new_pos_y;
+	double	new_pos_x;
+	double	new_pos_y;
 
-    new_pos_x = ft_get_new_position(player->plane.x);
-    new_pos_y = ft_get_new_position(player->plane.y);
-    if (ft_can_move(map, new_pos_x + player->pos.x, player->pos.y))
-        player->pos.x -= new_pos_x;
-    if (ft_can_move(map, player->pos.x, new_pos_y + player->pos.y))
-        player->pos.y -= new_pos_y;
+	new_pos_x = ft_get_new_position(player->plane.x);
+	new_pos_y = ft_get_new_position(player->plane.y);
+	if (ft_can_move(map, new_pos_x + player->pos.x, player->pos.y))
+		player->pos.x -= new_pos_x;
+	if (ft_can_move(map, player->pos.x, new_pos_y + player->pos.y))
+		player->pos.y -= new_pos_y;
 }
 
-void ft_move_right(t_player *player, char **map)
+void	ft_move_right(t_player *player, char **map)
 {
-    double new_pos_x;
-    double new_pos_y;
+	double	new_pos_x;
+	double	new_pos_y;
 
-    new_pos_x = ft_get_new_position(player->plane.x);
-    new_pos_y = ft_get_new_position(player->plane.y);
-    if (ft_can_move(map, new_pos_x + player->pos.x, player->pos.y))
-        player->pos.x += new_pos_x;
-    if (ft_can_move(map, player->pos.x, new_pos_y + player->pos.y))
-        player->pos.y += new_pos_y;
+	new_pos_x = ft_get_new_position(player->plane.x);
+	new_pos_y = ft_get_new_position(player->plane.y);
+	if (ft_can_move(map, new_pos_x + player->pos.x, player->pos.y))
+		player->pos.x += new_pos_x;
+	if (ft_can_move(map, player->pos.x, new_pos_y + player->pos.y))
+		player->pos.y += new_pos_y;
 }
 
 // static double ft_calc_dir_rotation()
@@ -78,28 +89,28 @@ void ft_move_right(t_player *player, char **map)
 // {
 
 // }
-void ft_rotate_left(t_player *player)
+void	ft_rotate_left(t_player *player)
 {
-    double actual_dir_x;
-    double actual_plane_x;
+	double	actual_dir_x;
+	double	actual_plane_x;
 
-    actual_dir_x = player->dir.x;
-    actual_plane_x = player->plane.x;
-    player->dir.x = player->dir.x * cos(-ROT_SPEED) - player->dir.y * sin(-ROT_SPEED);
-    player->dir.y = actual_dir_x * sin(-ROT_SPEED) + player->dir.y * cos(-ROT_SPEED);
-    player->plane.x = player->plane.x * cos(-ROT_SPEED) - player->plane.y * sin(-ROT_SPEED);
-    player->plane.y = actual_plane_x * sin(-ROT_SPEED) + player->plane.y * cos(-ROT_SPEED);
+	actual_dir_x = player->dir.x;
+	actual_plane_x = player->plane.x;
+	player->dir.x = player->dir.x * cos(-ROT_SPEED) - player->dir.y * sin(-ROT_SPEED);
+	player->dir.y = actual_dir_x * sin(-ROT_SPEED) + player->dir.y * cos(-ROT_SPEED);
+	player->plane.x = player->plane.x * cos(-ROT_SPEED) - player->plane.y * sin(-ROT_SPEED);
+	player->plane.y = actual_plane_x * sin(-ROT_SPEED) + player->plane.y * cos(-ROT_SPEED);
 }
 
-void ft_rotate_right(t_player *player)
+void	ft_rotate_right(t_player *player)
 {
-    double actual_dir_x;
-    double actual_plane_x;
+	double	actual_dir_x;
+	double	actual_plane_x;
 
-    actual_dir_x = player->dir.x;
-    actual_plane_x = player->plane.x;
-    player->dir.x = player->dir.x * cos(ROT_SPEED) - player->dir.y * sin(ROT_SPEED);
-    player->dir.y = actual_dir_x * sin(ROT_SPEED) + player->dir.y * cos(ROT_SPEED);
-    player->plane.x = player->plane.x * cos(ROT_SPEED) - player->plane.y * sin(ROT_SPEED);
-    player->plane.y = actual_plane_x * sin(ROT_SPEED) + player->plane.y * cos(ROT_SPEED);
+	actual_dir_x = player->dir.x;
+	actual_plane_x = player->plane.x;
+	player->dir.x = player->dir.x * cos(ROT_SPEED) - player->dir.y * sin(ROT_SPEED);
+	player->dir.y = actual_dir_x * sin(ROT_SPEED) + player->dir.y * cos(ROT_SPEED);
+	player->plane.x = player->plane.x * cos(ROT_SPEED) - player->plane.y * sin(ROT_SPEED);
+	player->plane.y = actual_plane_x * sin(ROT_SPEED) + player->plane.y * cos(ROT_SPEED);
 }
--- src/mandatory/player/player.h
+++ src/mandatory/player/player.h
@@ -6,7 +6,7 @@
 /*   By: aramirez <aramirez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 11:29:31 by amurcia-          #+#    #+#             */
-/*   Updated: 2022/11/23 16:34:46 by aramirez         ###   ########.fr       */
+/*   Updated: 2022/11/25 16:48:30 by aramirez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,31 +20,34 @@
 
 typedef struct s_movement
 {
-    bool forward;
-    bool backward;
-    bool left;
-    bool right;
-} t_movement;
+	bool	forward;
+	bool	backward;
+	bool	left;
+	bool	right;
+	float	speed;
+}	t_movement;
 
 typedef struct s_rotation
 {
-    bool left;
-    bool right;
-} t_rotation;
+	bool	left;
+	bool	right;
+	float	speed;
+}	t_rotation;
 
 typedef struct s_player
 {
-    t_vector dir;
-    t_vector pos;
-    t_vector plane;
-    t_movement movement;
-    t_rotation rotation;
-} t_player;
+	t_vector	dir;
+	t_vector	pos;
+	t_vector	plane;
+	t_movement	movement;
+	t_rotation	rotation;
+}	t_player;
 
-void ft_move_forward(t_player *player, char **map);
-void ft_move_backward(t_player *player, char **map);
-void ft_move_left(t_player *player, char **map);
-void ft_move_right(t_player *player, char **map);
-void ft_rotate_left(t_player *player);
-void ft_rotate_right(t_player *player);
+void	ft_move_forward(t_player *player, char **map);
+void	ft_move_backward(t_player *player, char **map);
+void	ft_move_left(t_player *player, char **map);
+void	ft_move_right(t_player *player, char **map);
+void	ft_rotate_left(t_player *player);
+void	ft_rotate_right(t_player *player);
+
 #endif
