592a70abbbfd46b27d7776aab12f9c202cbf01d9
aliciamurma <102992210+aliciamurma@users.noreply.github.com>
2022-11-25 22:07:00 +0100

    in_progress

--- src/mandatory/maps/maps.h
+++ src/mandatory/maps/maps.h
@@ -6,21 +6,21 @@
 /*   By: amurcia- <amurcia-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 11:35:43 by amurcia-          #+#    #+#             */
-/*   Updated: 2022/11/25 21:34:25 by amurcia-         ###   ########.fr       */
+/*   Updated: 2022/11/25 22:06:08 by amurcia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAPS_H
 # define MAPS_H
 
-typedef enum s_map_info {
-	NO='NO',
-	SO='SO',
-	WE='WE',
-	EA='EA',
-	C='C',
-	F='F'
-}	t_map_info;
+// typedef enum s_map_info {
+// 	NO='NO',
+// 	SO='SO',
+// 	WE='WE',
+// 	EA='EA',
+// 	C='C',
+// 	F='F'
+// }	t_map_info;
 
 typedef struct s_map
 {
@@ -32,14 +32,10 @@ typedef struct s_map
 
 // CHECK
 void	ft_check_parameters(int argc, char *map);
-void	ft_set_cub_info(t_game *game, char *cub);
 
 // HELPERS
 char	**ft_colour_split(char *str);
 char	**ft_words_split(char *str);
 void	ft_free_matrix(char **str);
-
-// INFO CUB
-void	ft_get_textures_colours(t_map map, t_texture texture);
 
 #endif
