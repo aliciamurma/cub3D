6080b83e795930583fee4dce3e294fb8aeba1b2e
Alberto Ramirez <57710147+arsalas@users.noreply.github.com>
2022-12-05 18:09:05 +0100

    Update render_weapon_bonus.c

--- src/bonus/render/render_weapon_bonus.c
+++ src/bonus/render/render_weapon_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aramirez <aramirez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 16:59:11 by aramirez          #+#    #+#             */
-/*   Updated: 2022/12/05 18:03:09 by aramirez         ###   ########.fr       */
+/*   Updated: 2022/12/05 18:08:57 by aramirez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,26 +21,15 @@ void	ft_render_weapon(void *mlx, void *win)
 {
 	t_image	img;
 	static int n = 0;
-	t_int_vector v;
+	// t_int_vector v;
 
 
 	n++;
-	img = ft_create_img(mlx, "./src/assets/weapons/weapon.xpm");
+	img = ft_create_img(mlx, "./src/assets/weapons/new_weapon.xpm");
 	printf("img: %i\n", img.width/2);
 	printf("wind: %i\n", WIDTH/2);
 	printf("start: %i\n", WIDTH/2 - img.width/2);
 
-	// mlx_put_image_to_window(mlx, win,
-	// 	img.pointer, WIDTH / 2 - img.width / 2, HEIGHT - img.height);
-	for (int i = 0; i < img.width; i++)
-	{
-		for (int j = 0; j < img.height; j++)
-		{
-			v.x = j;
-			v.y = i;
-			int c = ft_get_color_image(img, v, 1);
-			mlx_pixel_put(mlx, win, (WIDTH / 2 - img.width / 2) + i, HEIGHT - 1 - j, c);
-		}
-		
-	}
+	mlx_put_image_to_window(mlx, win,
+		img.pointer, WIDTH / 2 - img.width / 2, HEIGHT - img.height);
 }
