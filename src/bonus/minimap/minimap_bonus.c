a4eb1f9c1dd5fdadaaeb792eb93dea5e7e168983
aliciamurma <102992210+aliciamurma@users.noreply.github.com>
2022-12-07 10:14:50 +0100

    mal_map

--- src/bonus/minimap/minimap_bonus.c
+++ src/bonus/minimap/minimap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: amurcia- <amurcia-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 18:24:44 by amurcia-          #+#    #+#             */
-/*   Updated: 2022/12/07 10:08:47 by amurcia-         ###   ########.fr       */
+/*   Updated: 2022/12/07 10:14:36 by amurcia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,6 +56,47 @@
 // 	mlx_destroy_image(mlx.mlx_ptr, img2.pointer);
 // }
 
+// void	ft_render_minimap(t_window mlx, t_cub cub, t_vector pos)
+// {
+// 	int		x;
+// 	int		y;
+// 	int		c;
+// 	t_image	img;
+// 	t_image	img1;
+// 	t_image	img2;
+
+// 	(void)pos;
+// 	y = 0;
+// 	img1 = ft_create_img(mlx.mlx_ptr, "./src/assets/maps/bonus/minimap1_n.xpm");
+// 	img2 = ft_create_img(mlx.mlx_ptr, "./src/assets/maps/bonus/minimap2.xpm");
+// 	img = ft_create_new_img(mlx.mlx_ptr, 200, 200);
+// 	while (cub.map[y])
+// 	{
+// 		x = 0;
+// 		while (cub.map[y][x])
+// 		{
+// 			if (cub.map[y][x] == '0' || ft_is_player(cub.map[y][x]))
+// 			{
+// 				c = *(unsigned int *)(img.pixels
+// 					+ (y * 20 * img1.line_size + x
+// 						* 20 * (img1.bits_per_pixel / 8)));
+// 				ft_render_pixel(&img1, x, y, c);
+// 			}
+// 			else if (cub.map[y][x] != ' ')
+// 			{
+// 				c = *(unsigned int *)(img2.pixels
+// 					+ (y * 20 * img2.line_size + x
+// 						* 20 * (img2.bits_per_pixel / 8)));
+// 				ft_render_pixel(&img2, x, y, c);
+// 			}
+// 			x++;
+// 		}
+// 		y++;
+// 	}
+// 	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr,
+// 		img.pointer, 20 * x, 20 * y);
+// 	mlx_destroy_image(mlx.mlx_ptr, img.pointer);
+// }
 void	ft_render_minimap(t_window mlx, t_cub cub, t_vector pos)
 {
 	int		x;
@@ -78,15 +119,13 @@ void	ft_render_minimap(t_window mlx, t_cub cub, t_vector pos)
 			if (cub.map[y][x] == '0' || ft_is_player(cub.map[y][x]))
 			{
 				c = *(unsigned int *)(img.pixels
-					+ (y * 20 * img1.line_size + x
-						* 20 * (img1.bits_per_pixel / 8)));
+					+ (20 * img1.line_size + 20 * (img1.bits_per_pixel / 8)));
 				ft_render_pixel(&img1, x, y, c);
 			}
 			else if (cub.map[y][x] != ' ')
 			{
 				c = *(unsigned int *)(img2.pixels
-					+ (y * 20 * img2.line_size + x
-						* 20 * (img2.bits_per_pixel / 8)));
+					+ (20 * img2.line_size + 20 * (img2.bits_per_pixel / 8)));
 				ft_render_pixel(&img2, x, y, c);
 			}
 			x++;
@@ -94,6 +133,6 @@ void	ft_render_minimap(t_window mlx, t_cub cub, t_vector pos)
 		y++;
 	}
 	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr,
-		img.pointer, 20 * x, 20 * y);
+		img.pointer, 20, 20);
 	mlx_destroy_image(mlx.mlx_ptr, img.pointer);
 }
