6c2c2f9bb4ea90d9a9e9a345696b0b4570a62b44
aliciamurma <102992210+aliciamurma@users.noreply.github.com>
2022-12-07 10:27:42 +0100

    map_mal

--- src/bonus/minimap/minimap_bonus.c
+++ src/bonus/minimap/minimap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: amurcia- <amurcia-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 18:24:44 by amurcia-          #+#    #+#             */
-/*   Updated: 2022/12/07 10:23:39 by amurcia-         ###   ########.fr       */
+/*   Updated: 2022/12/07 10:27:37 by amurcia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -121,14 +121,14 @@ void	ft_render_minimap(t_window mlx, t_cub cub, t_vector pos)
 				c = *(unsigned int *)(img1.pixels
 					+ (y * img1.width * img1.line_size + x
 						* img1.width * (img1.bits_per_pixel / 8)));
-				ft_render_pixel(&img1, 20, 20, c);
+				ft_render_pixel(&img1, x, y, c);
 			}
 			else if (cub.map[y][x] != ' ')
 			{
 				c = *(unsigned int *)(img2.pixels
 					+ (y * img2.width * img2.line_size + x
 						* img2.width * (img2.bits_per_pixel / 8)));
-				ft_render_pixel(&img2, 20, 20, c);
+				ft_render_pixel(&img2, x, y, c);
 			}
 			x++;
 		}
